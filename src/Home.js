f2789147294560b669c2ff6f63338d3ad8c5eea2
David Tunnell <davidtunnell9@gmail.com>
2021-10-12 15:21:05 -0500

    added click events and ability to pass events and parameters in them

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log(e);
+    };
+
+    const handleClickAgain = (e, name) => {
+        console.log(name);
+        console.log(e.target);
+    };
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button
+                //wrap the click handler in an anonymous arrow function to pass a parameter without it auto running
+                onClick={(e) => {
+                    handleClickAgain(e, "Dizzle");
+                }}
+            >
+                Click me again
+            </button>
         </div>
     );
 };
