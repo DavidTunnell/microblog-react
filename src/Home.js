305c53133158da85b9e07b5dfa02cfc263987716
David Tunnell <davidtunnell9@gmail.com>
2021-10-12 16:30:19 -0500

    added state hooks to home

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,25 @@
-const Home = () => {
-    const handleClick = (e) => {
-        console.log(e);
-    };
+import { useState } from "react";
 
-    const handleClickAgain = (e, name) => {
-        console.log(name);
-        console.log(e.target);
+const Home = () => {
+    //the state of a component refers to the data being used by a component at a point in time
+    //you must use state and not variables because it wont update the page
+    //this is because you must use reactive variables (variables that react looks at to check/watch for state change)
+    //this is done using hooks (useState hook in particular)
+    const [name, setName] = useState("Dizzle");
+    const [age, setAge] = useState(25);
+
+    const handleClick = (e) => {
+        setName("Dizzle Tizzle"); //this set update is reactive
+        setAge(30);
     };
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>
+                {name} is {age} years old.
+            </p>
             <button onClick={handleClick}>Click me</button>
-            <button
-                //wrap the click handler in an anonymous arrow function to pass a parameter without it auto running
-                onClick={(e) => {
-                    handleClickAgain(e, "Dizzle");
-                }}
-            >
-                Click me again
-            </button>
         </div>
     );
 };
