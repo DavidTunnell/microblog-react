0bfde768f2f646b057069c25652ae7df14c0ee80
David Tunnell <davidtunnell9@gmail.com>
2021-10-13 14:28:38 -0500

    added react-route-dom to project and implemented it for <Home /> component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//top use the react router package, surround the whole app with the router component
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <Home />
+        <Router>
+            {/* ^ Here the app is wrapped with the router */}
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    {/* Next is the decision of where page content to go based on different routes
+                        All routes go in the switch component so only one renders at a time based on route  */}
+                    <Switch>
+                        {/* add a route for each component AND the component itself nested*/}
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
