162010117352daf3a9acc60b800e6ecc268bdaa5
David Tunnell <davidtunnell9@gmail.com>
2021-10-14 11:17:17 -0500

    added call using custom hook and rendering data for blog details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 const BlogDetails = () => {
     //this allows ups to grab parameters from the URL routes with destructuring
     const { id } = useParams();
+    const {
+        data: blog,
+        error,
+        isPending,
+    } = useFetch("http://localhost:8000/blogs/" + id);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details</h2>
-            <p>{id}</p>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 };
